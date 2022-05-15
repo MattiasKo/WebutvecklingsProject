6f0dfc3217af5e914e94f2c35064989129574ee4
MattiasKo <91311332+MattiasKo@users.noreply.github.com>
2022-05-15 20:02:24 +0200

    Add files via upload

---
+++ js/info.js
@@ -0,0 +1,12 @@
+function myFunction1() {
+    document.getElementById("myinfo1").innerHTML = "En bankomat om första kursen";
+}
+function myFunction2() {
+    document.getElementById("myinfo1").innerHTML = "Ett C# project där man ska gissa numret, ganska roligt.";
+}
+function myFunction3() {
+    document.getElementById("myinfo1").innerHTML = "Programmerat advancerad .net";
+}
+function myFunction4() {
+    document.getElementById("myinfo1").innerHTML = "Gjort en applications med en databas som har elever lärare och kurser";
+}
